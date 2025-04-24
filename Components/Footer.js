a6aa197a908cd6ca3f472560d2cd17f61f19e5de
H2o <131161445+PhonlatatKathintip@users.noreply.github.com>
2025-04-25 03:25:31 +0700

    startProp

---
+++ Components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="flex items-start justify-center mx-auto min-h-screen ">
+      <div className="max-w-md mx-auto bg-white rounded-b-md shadow-md overflow-hidden my-4">
+        <div className="p-4 bg-gray-100">
+          <div className="mt-1 text-lg text-center font-medium text-gray-600">
+            Happy Birthday Song
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
 import Custom from "@/Components/Custom";
 import MathS from "@/Components/MathS";
+import Footer from "@/Components/Footer";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Custom />
-      <div>
-        <MathS />
-      </div>
+    <div className="flex items-center justify-center min-h-screen mx-1 my-1 ">
+      This is page
     </div>
   );
 }
