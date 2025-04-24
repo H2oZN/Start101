99e10e6c1fbbdafaf278a692d07a8da12ce61aa5
H2o <131161445+PhonlatatKathintip@users.noreply.github.com>
2025-04-24 20:32:18 +0700

    testtailwind

--- Components/Custom.js
+++ Components/Custom.js
@@ -1,5 +1,10 @@
 import React from "react";
+import "@/app/styles.css";
 
 export default function Custom() {
-  return <div>Thisis data</div>;
+  return (
+    <div className="container">
+      <div id="main">Thisis data</div>
+    </div>
+  );
 }
--- Components/MathS.js
+++ Components/MathS.js
@@ -1,11 +1,12 @@
 import React from "react";
+import classes from "@/Components/Maths.modules.css";
 
 export default function MathS() {
   const Name = "H2o";
   const Mid = 20;
   const Final = 30;
   return (
-    <div className="container">
+    <div className="classes.container">
       <h1>Student Score</h1>
       <h3>{Name}</h3>
       <p>total Score = {Mid + Final}</p>
