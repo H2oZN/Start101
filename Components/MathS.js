c7a731706fa4da878e3f66b34fabf1a58927b887
H2o <131161445+PhonlatatKathintip@users.noreply.github.com>
2025-04-24 21:06:30 +0700

    finishtailwind01

--- Components/MathS.js
+++ Components/MathS.js
@@ -6,7 +6,7 @@ export default function MathS() {
   const Mid = 20;
   const Final = 30;
   return (
-    <div className="classes.container">
+    <div className="text-3xl text-amber-200 non-italic text-center underline font-mono leading uppercase w-200 m-10">
       <h1>Student Score</h1>
       <h3>{Name}</h3>
       <p>total Score = {Mid + Final}</p>
