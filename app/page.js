1ea8afa64c567ca693e73b109eb27ca1e6cdd343
H2oZN <Phonlatues.w@gmail.com>
2025-04-27 20:57:59 +0700

    Start button handler

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Footer from "@/Components/Footer";
 import Header from "@/Components/Header";
 import Allproduct from "@/Components/Allproduct";
+import MyWrap from "@/Components/MyWrapper";
 
 export default function Home() {
   return (
@@ -9,7 +10,13 @@ export default function Home() {
       <body>
         <div>
           <div className="flex flex-col justify-center items-center min-h-screen ">
-            <Allproduct />
+
+
+            {/* <Allproduct /> */}
+            {/* <MyWrap>Hello</MyWrap>
+            <MyWrap>This is my world</MyWrap> */}
+
+
             
           </div>
         </div>
